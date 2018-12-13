3e36433dd121afaf0c6d972c19d11b330daebc46
Abarnaalex <45810884+Abarnaalex@users.noreply.github.com>
2018-12-13 11:22:09 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,12 +1,38 @@
 funtion validateform(){
+x=document.getElementBYId("username").value;
+if(x==""){
+alert("username must be filled");
+return false;
+}
 x=document.getElementBYId("email").value;
 if(x==""){
 alert("email must be filled");
 return false;
 }
-x=document.gerElementBYId("password").value;
+
+x=document.getElementBYId("password").value;
 if(x==""){
 alert("password must be filled");
+return false;
+}
+x=document.getElementBYId("Degree").value;
+if(x==""){
+alert("Degree must be filled");
+return false;
+}
+x=document.getElementBYId("Branch").value;
+if(x==""){
+alert("Branch must be filled");
+return false;
+}
+x=document.getElementBYId("Collegename").value;
+if(x==""){
+alert("Collegename must be filled");
+return false;
+}
+x=document.getElementBYId("repeate password").value;
+if(x==""){
+alert("repeate password must be filled");
 return false;
 }
 }
